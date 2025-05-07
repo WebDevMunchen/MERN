e986580081878296134a93228439455f8fd13397
unknown <denis991280@gmail.com>
2025-05-07 14:05:55 +0200

    step9

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,5 +1,40 @@
+import { useForm } from "react-hook-form";
+import axios from "axios"
+
 export default function Login() {
-    return(
-        <h1>Login</h1>
-    )
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    axios
+      .post("http://localhost:8000/user/login", data)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <>
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input defaultValue="test@test.com" {...register("email")} />
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <input type="password" {...register("password", { required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+
+        <input type="submit" />
+      </form>
+    </>
+  );
 }
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,5 +1,44 @@
+import axios from "axios";
+import { useForm } from "react-hook-form";
+
 export default function Register() {
-    return(
-        <h1>Register</h1>
-    )
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    axios
+      .post("http://localhost:8000/user/register", data)
+      .then((response) => {
+        console.log("Success!");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <>
+      <h1>Register</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input
+          type="email"
+          defaultValue="test@test.com"
+          {...register("email")}
+        />
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <input type="password" {...register("password", { required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+
+        <input type="submit" />
+      </form>
+    </>
+  );
 }
