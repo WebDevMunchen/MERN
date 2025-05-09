fde90eefe9b5e78390336f54ba1a7ceee12ccce3
unknown <denis991280@gmail.com>
2025-05-09 14:25:07 +0200

    step3

--- routes/user-route.js
+++ routes/user-route.js
@@ -17,7 +17,7 @@ userRouter.route("/register").post(registerUser);
 userRouter.route("/login").post(login);
 userRouter.route("/logout").put(logout);
 userRouter.route("/getUser/:id").get(getUser);
-userRouter.route("/getProfile/:id").get(authenticate, getProfile);
+userRouter.route("/getProfile").get(authenticate, getProfile);
 userRouter.route("/updateUser/:id").put(updateUser);
 userRouter.route("/deleteUser/:id").delete(deleteUser);
 
